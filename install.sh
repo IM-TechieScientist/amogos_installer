bc5f64c0b2fa108277a80a098ddd453bb57dafc7
Santosh <84835176+IM-TechieScientist@users.noreply.github.com>
2021-07-15 15:44:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ echo "Please change theme to arc-dark or arc as per your wish"
 echo "Please change the cursor and icon packs as per your wish as well(breeze and papirus)"
 read -n 1 -s -r -p "Press any key to continue"
 echo ""
-/usr/share/appearance/lxappearance.desktop
+lxappearance
 
 figlet "Installation has completed!"
 
